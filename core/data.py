6e732e4ec5b5f381d15ec03b3a0236c4452a1276
PSongz <43245094+PSongz@users.noreply.github.com>
2025-01-13 13:37:14 +0100

    Fixing static files path v2

--- core/data.py
+++ core/data.py
@@ -4,7 +4,8 @@ import json
 from django import template
 import pickle
 import xgboost as xgbl
-
+from django.contrib.staticfiles.finders import find
+import os
 
 # Function replace
 register = template.Library()
@@ -44,7 +45,7 @@ time_series = {
 }
 
 # Map JSON
-with open(r'js\Europe_countries_shp_custom.json', 'r', encoding='utf-8') as f:
+with open(find('js/Europe_countries_shp_custom.json'), 'r', encoding='utf-8') as f:
     geojson_data = json.load(f)
 
 for k in range(len(geojson_data['features'])):
@@ -52,12 +53,12 @@ for k in range(len(geojson_data['features'])):
 
 
 ########## Data for the Bank Churn rate project ############
-cr_data = pd.read_csv(r"data\train.csv")
+cr_data = pd.read_csv(find("data/train.csv"))
 cr_cat_variables = ['Geography', 'Gender', 'HasCrCard', 'IsActiveMember', 'NumOfProducts', 'Tenure']
 cr_data_describe = pd.concat([cr_data.describe(include='all'), cr_data.isnull().sum(axis=0).to_frame(name="missing_values").T], axis=0).round(2).drop(index=['unique', 'top', 'freq'])
 cr_data = cr_data.drop(columns=['Surname', 'CustomerId', 'id'])
 
-with open(r"models\bank_churnrate_xgboost.pkl", 'rb') as file:  
+with open(find("models/bank_churnrate_xgboost.pkl"), 'rb') as file:  
     model = pickle.load(file)
 
 # Extraire les importances des features
