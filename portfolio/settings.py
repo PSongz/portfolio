4c7e573dbb589e6b43831062e23dc542cfec3559
PSongz <43245094+PSongz@users.noreply.github.com>
2025-01-13 18:00:12 +0100

    add print for debugging

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,6 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+print(f"WAAAAAAAAAAAAZAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA SECRET_KEY: {os.getenv('SECRET_KEY')}")
+
 #SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
