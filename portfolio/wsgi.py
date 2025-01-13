00ce9cf3a1bb88e223a5a3c7838f48f753168d45
PSongz <43245094+PSongz@users.noreply.github.com>
2025-01-13 17:28:26 +0100

    loading the SECRET_KEY in wsgi.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
 
 application = get_wsgi_application()
